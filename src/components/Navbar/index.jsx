d0e2eae63e0f59539177f236fa58160ee3873775
User <mamatovmurod8898@gmail.com>
2024-08-22 01:53:26 +0500

    navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,10 @@
-import React,  {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
-import { NavLink, Outlet} from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
-import { Container,Box , List, ListItme, Btn, Logo, Logojon, Nav, NavLeft, NavRight, Selekt, Link} from './style.jsx'
+import Remu from "../../assets/img/Remove.png"
+
+import { Container, Box, List, ListItme, Btn, Logo, Logojon, Nav, NavLeft, NavRight, Selekt, Link, NavMadya, ImgMad, ListMad } from './style.jsx'
 import "./index.css"
 import Download from '../Download/index.jsx'
 // import Logojon  from '../../assets/img/logo.png'
@@ -10,20 +12,37 @@ import Download from '../Download/index.jsx'
 import Pochta from "../../assets/icons/mail-fill.png"
 import Telifon from "../../assets/icons/phone-fill.png"
 
+
 const Navbar = () => {
+
   
-const [count, setCount ] = useState(false)
 
-const [shrink, setShrink] = useState(false);
 
-useEffect(() => {
+
+  const [count, setCount] = useState(false)
+
+  const [navburger, setNavburger] = useState(false)
+
+  const [shrink, setShrink] = useState(false);
+
+  let Windov =()=>{
+    window.onclick(()=>{
+      setNavburger(false)
+    })
+  }
+
+
+
+
+
+  useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 50) { // 50px dan ko'p skroll qilingan bo'lsa
         setShrink(true);
-        
+
       } else {
         setShrink(false);
-        
+
       }
     };
 
@@ -33,56 +52,89 @@ useEffect(() => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-  
-const  ButtonClik=()=>{
 
-   setCount(!count);
-}
+  const ButtonClik = () => {
+
+    const Nav = () => {
+      setNavburger(!navburger)
+    }
+
+    setCount(!count);
+  }
+
+
+  const NavMad = () => {
+    return <NavMadya>
+      <ImgMad onClick={() => setNavburger(false) } src={Remu} alt="" />
+      <ListMad>
+        <ListItme>Home</ListItme>
+        <ListItme>About</ListItme>
+        <ListItme>Features</ListItme>
+        <ListItme>Screenshot</ListItme>
+        <ListItme>Blog</ListItme>
+      </ListMad>
+    </NavMadya>
+  }
+
 
 
 
   return (
-     <div>
-        <Container>
+    <div>
+      {navburger ? <NavMad /> : <></>}
+      <Container>
         <Box>
 
-        
-          <Nav className ={shrink?"none":  "flex"}>
+
+          <Nav className={shrink ? "none" : "flex"}>
             <NavLeft>
               <img src={Pochta} width={18} alt="" />
               <Link href="#">Info@youremail.com</Link>
             </NavLeft>
 
             <NavRight>
-            <img src={Telifon} width={18} alt="" />
-            <Link href="#">+998 __ --- -- --</Link>
+              <img src={Telifon} width={18} alt="" />
+              <Link href="#">+998 __ --- -- --</Link>
             </NavRight>
           </Nav>
 
-        <List>
-          <ListItme>  Home     </ListItme>
-          <ListItme>  About    </ListItme>
-          <ListItme>  Features </ListItme>
-
-          <ListItme className='logojon'> 
-            <span> </span> 
-             <Logo src={Logojon}   alt="" />
-          </ListItme>
+          <List>
+            <ListItme className='GamburgerMenyu' onClick={() => setNavburger(true)} >
+              <div>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z" fill="#232233" />
+                </svg>
+              </div>
 
 
-          <ListItme>  Screenshot </ListItme>
 
-          
+            </ListItme>
 
-          <ListItme>  Blog </ListItme>
-          <ListItme>  
-             <Btn type="primary" onClick={ButtonClik} >Download {count&&<Selekt><Download prop="colmun"></Download> </Selekt>} </Btn>
-          </ListItme>
-        </List>
+            <ListItme className='navNan'>  Home     </ListItme>
+            <ListItme className='navNan'>  About    </ListItme>
+            <ListItme className='navNan'>  Features </ListItme>
+
+            <ListItme className='logojon'>
+              <span> </span>
+              <Logo src={Logojon} alt="" />
+            </ListItme>
+
+
+            <ListItme className='navNan'>  Screenshot </ListItme>
+
+
+
+            <ListItme className='navNan'>  Blog </ListItme>
+            <ListItme>
+              <Btn type="primary" onClick={ButtonClik} >Download {count && <Selekt><Download prop="colmun"></Download> </Selekt>} </Btn>
+            </ListItme>
+          </List>
         </Box>
-        </Container>
+      </Container>
 
-           <Outlet />
+
+
+      <Outlet />
     </div>
   )
 }
--- src/components/Navbar/style.jsx
+++ src/components/Navbar/style.jsx
@@ -45,12 +45,14 @@ const List = styled.ul`
     justify-content: space-between;
     background-color: var(--colorPrimary);
     transition: all .5s ease;
-
+    position: relative;
+    box-shadow: 5px 5px  10px black;
+    background-color: #ffcccc;
+   
 `
 
 const ListItme = styled.li`
-  
-   position: relative;
+  cursor: pointer;
    
 
 `
@@ -59,6 +61,16 @@ const Btn = styled(Button)`
     width: 150px;
     height: 47px;
     position: relative;
+    @media (max-width: 576px) {
+    width: 76px;
+    height: 34px;
+   
+    font-size: 10px;
+    font-weight: 600;
+    line-height: 10px;
+    text-align: left;
+
+    }
 `
 const Logo= styled.img`
 
@@ -66,7 +78,7 @@ const Logo= styled.img`
     border-radius: 5px 0px 0px 0px;
     
     position: absolute;
-    top:80%;
+    top:50%;
     left:50%;
     transform:translate(-50%, -50%);
 
@@ -77,6 +89,10 @@ const Nav = styled.nav`
     display: flex;
     margin-bottom: 10px;
     transition: all .5s ease;
+
+    @media (max-width: 991px) {
+    display: none;
+    }
 `
 const NavLeft = styled.nav`
     display: flex;
@@ -110,11 +126,45 @@ const  Link = styled.a`
     font-weight: 400;
     line-height: 24px;
     text-align: left;
-    color: #fff;
+    color: #ffffff;
     text-decoration: none;
    
 
 `
+const NavMadya = styled.div`
+    width: 270px;
+    padding: 67px 25px 40px 40px;
+    z-index: 200;
+    background-color: rgba(88, 86, 233, 0.8);
+    height:100vh;
+    z-index:22222;
+    position: fixed;
+    top: 0px;
+    left: 0;
+`
+const ImgMad = styled.img`
+    text-align: right;
+    cursor: pointer;
+    position: absolute;
+    top: 30px;
+    right: 30px;
+`
+
+const ListMad = styled.div`
+
+        display: flex;
+        flex-direction:column;
+        gap:25px;
+        color: #fff;
+        font-size: 20px;
+        font-weight: 600;
+        line-height: 30px;
+        text-align: left;
+        list-style: none;
+        margin: 0;
+        padding: 0;
+       
+`
 
 
-export { Container, Box ,List, ListItme, Btn , Logo, Logojon, Nav, NavLeft, NavRight, Selekt,Link}
+export { Container, Box ,List, ListItme, Btn , Logo, Logojon, Nav, NavLeft, NavRight, Selekt,Link,  NavMadya, ImgMad, ListMad}
